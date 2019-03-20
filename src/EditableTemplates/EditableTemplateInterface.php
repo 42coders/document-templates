c494348ba4b8713e3f27df2cc53abb0d07160044
Daniel Verner <daniel.verner@42coders.com>
2019-03-20 18:05:39 +0100

    extend the editable template interface

--- src/EditableTemplates/EditableTemplateInterface.php
+++ src/EditableTemplates/EditableTemplateInterface.php
@@ -14,4 +14,14 @@ interface EditableTemplateInterface
      * @return mixed
      */
     public function setContent($content);
+
+    /**
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * @param string $name
+     */
+    public function setName($name);
 }
