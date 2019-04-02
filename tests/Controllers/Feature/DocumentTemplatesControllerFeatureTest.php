eb40fe0ee81b2ad0b896104d60c200cb828db5f4
Daniel Verner <daniel.verner@42coders.com>
2019-04-02 12:04:23 +0200

    split controller test unit/feature (http)

--- tests/Controllers/Feature/DocumentTemplatesControllerFeatureTest.php
+++ tests/Controllers/Feature/DocumentTemplatesControllerFeatureTest.php
@@ -1,16 +1,14 @@
 <?php
 
-namespace BWF\DocumentTemplates\Tests\Controllers;
+namespace BWF\DocumentTemplates\Tests\Controllers\Feature;
 
-use BWF\DocumentTemplates\DocumentTemplates;
 use BWF\DocumentTemplates\DocumentTemplates\DocumentTemplateModel;
-use BWF\DocumentTemplates\Http\Controllers\DocumentTemplatesController;
+use BWF\DocumentTemplates\Tests\Controllers\DemoDocumentTemplatesController;
+use BWF\DocumentTemplates\Tests\Controllers\FeatureTestCase;
 use BWF\DocumentTemplates\Tests\DocumentTemplates\DemoDocumentTemplate;
-use BWF\DocumentTemplates\Tests\DocumentTemplates\DemoDocumentTemplateModel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-class DocumentTemplatesControllerTest extends FeatureTestCase
+class DocumentTemplatesControllerFeatureTest extends FeatureTestCase
 {
     /**
      * @var \BWF\DocumentTemplates\Http\Controllers\DocumentTemplatesController
@@ -31,102 +29,6 @@ class DocumentTemplatesControllerTest extends FeatureTestCase
 
         $this->controller = new DemoDocumentTemplatesController();
         $this->request = new Request();
-    }
-
-
-    public function testTemplatesWithModel()
-    {
-        $request = new Request();
-
-        $documentTemplateModel = new DemoDocumentTemplateModel();
-        $documentTemplateModel->fill($this->documentTemplateData);
-
-        $templates = $this->controller->templates($request, $documentTemplateModel);
-
-        $this->assertCount(2, $templates);
-    }
-
-    public function testTemplatesWithOutModel()
-    {
-        $this->request->layout = 'TestLayout.html.twig';
-        $this->request->document_class = DemoDocumentTemplate::class;
-
-        $templates = $this->controller->templates($this->request);
-
-        $this->assertCount(2, $templates);
-    }
-
-    public function testPlaceholdersWithModel()
-    {
-        $documentTemplateModel = new DemoDocumentTemplateModel();
-        $documentTemplateModel->fill($this->documentTemplateData);
-
-        $placeholders = $this->controller->placeholders($this->request, $documentTemplateModel);
-
-        $this->assertCount(4, $placeholders);
-    }
-
-    public function testPlaceholdersWithOutModel()
-    {
-        $this->request->layout = 'TestIterableDataSource.html.twig';
-        $this->request->document_class = DemoDocumentTemplate::class;
-
-        $placeholders = $this->controller->placeholders($this->request);
-
-        $this->assertCount(4, $placeholders);
-    }
-
-    public function testGetAvailableClasses()
-    {
-        $class = new \ReflectionClass(DocumentTemplatesController::class);
-        $getAvailableClassesMethod = $class->getMethod('getAvailableClasses');
-        $getAvailableClassesMethod->setAccessible(true);
-        $documentClassesMember = $class->getProperty('documentClasses');
-        $documentClassesMember->setAccessible(true);
-
-        $classes = collect([
-            'class1',
-            'class2',
-            DemoDocumentTemplate::class
-        ]);
-
-        $documentTemplateData = [
-            'name' => '',
-            'document_class' => DemoDocumentTemplate::class,
-            'layout' => 'TestIterableDataSource.html.twig'
-        ];
-
-        $documentTemplateModel = new DocumentTemplateModel();
-        $documentTemplateModel->fill($documentTemplateData);
-        $documentTemplateModel->save();
-
-
-        $documentClassesMember->setValue($this->controller, $classes);
-        $availableClasses = $getAvailableClassesMethod->invoke($this->controller);
-
-        $expectedClasses = collect([
-                'class1',
-                'class2',
-            ]
-        );
-
-        $this->assertEquals($expectedClasses, $availableClasses);
-    }
-
-    public function testGetAvailableLayouts()
-    {
-        $class = new \ReflectionClass(DocumentTemplatesController::class);
-        $getAvailableLayoutsMethod = $class->getMethod('getAvailableLayouts');
-        $getAvailableLayoutsMethod->setAccessible(true);
-
-        $expectedLayouts = collect([
-            0 => "TestLayout.html.twig",
-            1 => "TestIterableDataSource.html.twig"
-        ]);
-
-        $availableLayouts = $getAvailableLayoutsMethod->invoke($this->controller);
-
-        $this->assertEquals($expectedLayouts, $availableLayouts);
     }
 
     public function testUpdate()
