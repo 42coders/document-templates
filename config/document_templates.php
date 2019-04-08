2f5c319d45b8b10f83eeb0e3a8661f1c509e93ac
Daniel Verner <daniel.verner@42coders.com>
2019-04-08 18:01:09 +0200

    Added sandbox to user editable templates.

--- config/document_templates.php
+++ config/document_templates.php
@@ -2,5 +2,18 @@
 
 return [
 
-    'layout_path' => resource_path('templates')
+    'layout_path' => resource_path('templates'),
+
+    /**
+     * Twig sandbox configuration
+     *
+     * @see https://twig.symfony.com/doc/2.x/api.html#sandbox-extension
+     */
+    'template_sandbox' => [
+        'allowedTags' => ['for'],
+        'allowedFilters' => ['escape'],
+        'allowedMethods' => [],
+        'allowedProperties' => [],
+        'allowedFunctions' => []
+    ]
 ];
