2a2da48322969bdfae29ee0cb54f5b310df0bf07
Daniel Verner <daniel.verner@42coders.com>
2019-07-03 10:14:56 +0200

    Add model class and base url to the config

--- config/document_templates.php
+++ config/document_templates.php
@@ -2,6 +2,9 @@
 
 return [
 
+    /**
+     * The base path to search for the layout twig files
+     */
     'layout_path' => resource_path('templates'),
 
     /**
@@ -32,5 +35,16 @@ return [
             'autoescape' => false,
             'optimizations' => -1
         ]
-    ]
+    ],
+
+    /**
+     * The model class to be used with route model binding, and in the controller
+     */
+    'model_class' => \BWF\DocumentTemplates\DocumentTemplates\DocumentTemplateModel::class,
+
+    /**
+     * Base url to use by the generate routes (e.g /document-templates/, /document-templates/1/edit).
+     * The routes are also named by this base url, and they could be used like: route('document.template.index')
+     */
+    'base_url' => 'document-templates'
 ];
