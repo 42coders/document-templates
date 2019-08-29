7af7b13ee3fece63f876f140b8fe288614887bf9
Daniel Verner <daniel.verner@42coders.com>
2019-08-29 13:53:36 +0200

    Cleanup

--- src/Renderers/RendererInterface.php
+++ src/Renderers/RendererInterface.php
@@ -2,17 +2,17 @@
 
 namespace BWF\DocumentTemplates\Renderers;
 
-use BWF\EditableTemplates\EditableTemplateInterface;
-use BWF\Layouts\LayoutInterface;
-use BWF\TemplateDataSources\TemplateDataSourceInterface;
+use BWF\DocumentTemplates\EditableTemplates\EditableTemplateInterface;
+use BWF\DocumentTemplates\Layouts\LayoutInterface;
+use BWF\DocumentTemplates\TemplateDataSources\TemplateDataSourceInterface;
 
 interface RendererInterface
 {
     /**
-     * @param \BWF\DocumentTemplates\Layouts\Layout $layout
+     * @param \BWF\DocumentTemplates\Layouts\LayoutInterface $layout
      * @param \BWF\DocumentTemplates\EditableTemplates\EditableTemplate[] $templates
      * @param \BWF\DocumentTemplates\TemplateDataSources\TemplateDataSource[] $data
      * @return mixed
      */
-    public function render($layout, $templates, $data);
+    public function render(LayoutInterface $layout, $templates, $data);
 }
